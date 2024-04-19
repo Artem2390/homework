ddd91e4f0693080361ee4ed7c52bf021b36e9083
Artem_Kuznetsov <kuzya6179@gmail.com>
2024-04-19 19:42:32 +0300

    first commit

---
+++ lesson_11/first_git.py
@@ -0,0 +1,24 @@
+"""
+  504  git remote add origin https://gitlab.com/usichenko.vlad67/cbs-git-module.git
+  505  got remote
+  506  git remote
+  507  git remote origin
+  508  git status
+  509  git push -u origin master
+  510  git status
+  511  git branch
+  512  git commit -m "New commit"
+  513  git add new_file.py
+  514* git
+  515  git push -u origin master
+  516  git branch
+  517  git pull origin
+  518  git branch
+  519  git branch origin
+  520  git checkout branch-from-gitlab
+  521  git add new_file.py
+  522  git commit -m "Antoher branch"
+  523  git push -u origin branch-from-gitlab
+  524  history
+
+"""
